fd851ff867e8504ed647f3fa7c934fd8511e2be3
Maxime <maxime.garnier@imt-atlantique.net>
2022-03-04 21:12:14 +0100

    backend : formulaire d'envoi de messages fonctionnel

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import flask
 from flask import Flask
 from database.database import db, init_database
-from database.models import Group, User, Message
+from database.models import Group, User, Message, group_user_table
 import flask
 
 
@@ -42,11 +42,16 @@ def create_message(content, user, group):
     db.session.commit()
     return msg
 
+#makes an user join a group
 def join_group(user, group):
     group.users.append(user)
     db.session.add(user)
     db.session.commit()
     return
+
+#checks if an user is in a group
+def is_in_group(user_id, group_id):
+    return db.session.query(group_user_table).filter_by(group_id=group_id, user_id=user_id).first() is not None
 
 @app.route('/messages')
 def messages():
@@ -72,6 +77,67 @@ def test():
     groups = Group.query.all()
 
     return flask.render_template('test.html.jinja2', groups=groups)
+
+@app.route('/send', methods=['POST','GET'])
+def form_send_msg():
+    form_is_valid, errors = is_form_valid(flask.request.form)
+    if not form_is_valid:
+        return show_form(flask.request.form, errors)
+    else:
+        return send_form(flask.request.form)
+    return flask.render_template('send.html.jinja2')
+
+#form is invalid if: user or group doesn't exist, user is not in group, msg is empty
+def is_form_valid(form):
+    result = True
+    errors = []
+
+    user_id = form.get('user_id', "")
+    if user_id == "":
+        result = False
+        errors += ['user field is empty']
+    else:
+        user = User.query.filter_by(id=user_id).first()
+        if user == "":
+            result = False
+            errors += ['user does not exist']
+
+    group_id = form.get('group_id', "")
+    if user_id == "":
+        result = False
+        errors += ['group field is empty']
+    else:
+        group = Group.query.filter_by(id=group_id).first()
+        if group == "":
+            result = False
+            errors += ['group does not exist']
+
+    content = form.get('msg', "")
+    if content == "":
+        result = False
+        errors += ['msg field is empty']
+
+    if result and not is_in_group(user_id, group_id):
+        result = False
+        errors += ['user in not in group']
+
+    print("############", errors)
+    return result, errors
+
+def show_form(form, errors):
+    return flask.render_template('send.html.jinja2', errors = errors)
+
+def send_form(form):
+    user = User.query.filter_by(id=form.get('user_id')).one()
+    group = Group.query.filter_by(id=form.get('group_id')).one()
+    msg = create_message(
+        content=form.get('msg'),
+        user=user,
+        group=group
+    )
+    return flask.render_template('send.html.jinja2')
+
+
 
 if __name__ == '__main__':
     app.run()
