2568afeb0a3daca1093a9188b27a1f28fae2a512
Maxime <maxime.garnier@imt-atlantique.net>
2022-03-08 17:21:38 +0100

    Prise en compte des sessions utilisateurs et création de fonctions delete

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import flask
 from flask import redirect, url_for
-from flask_login import login_required, LoginManager, login_user, current_user
+from flask_login import login_required, LoginManager, login_user, current_user, logout_user
 from database.database import db, init_database
 from database.models import Group, User, Message, group_user_table
 
@@ -12,7 +12,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'xxxxyyyyyzzzzz'
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'logintest'
+login_manager.login_view = 'login'
 
 db.init_app(app)
 
@@ -20,11 +20,38 @@ with app.test_request_context():
     init_database()
 
 #cleaning all db
-@app.route('/clean')
-def db_clean():
+@app.route('/clean/all')
+def db_clean_all():
     db.drop_all()
     db.create_all()
     return "Cleaned!"
+
+@app.route('/clean/<table>')
+def db_clean_table(table):
+    if table == 'users':
+        users = User.query.all()
+        for user in users:
+            delete_user(user)
+        return 'Users cleaned!'
+    elif table == 'groups':
+        groups = Group.query.all()
+        for group in groups:
+            delete_group(group)
+        return 'Groups cleaned!'
+    elif table == 'part':
+        group_user_table.drop()
+        db.create_all()
+        return 'Participations cleaned!'
+    elif table == 'msg' :
+        clean_model_table(Message)
+        return 'Messages cleaned !'
+    return "/clean/['users', 'groups', 'part', 'msg']"
+
+def clean_model_table(model):
+    for row in model.query.all():
+        db.session.delete(row)
+    db.session.commit()
+    return
 
 def create_user(name):
     user = User(name=name)
@@ -32,11 +59,33 @@ def create_user(name):
     db.session.commit()
     return user
 
+def delete_user(user):
+    #we have to delete the user, its messages and its participations
+    messages = Message.query.filter_by(sender_id=user.id).all()
+    for msg in messages:
+        db.session.delete(msg)
+    participations = db.session.query(group_user_table).filter_by(user_id=user.id).all()
+    for part in participations:
+        db.session.delete(part)
+    db.session.commit()
+    return
+
 def create_group(name):
     group = Group(name=name)
     db.session.add(group)
     db.session.commit()
     return group
+
+def delete_group(group):
+    #we have to delete the group, its messages and its participations
+    messages = Message.query.filter_by(group_id=group.id).all()
+    for msg in messages:
+        db.session.delete(msg)
+    participations = db.session.query(group_user_table).filter_by(group_id=group.id).all()
+    for part in participations:
+        db.session.delete(part)
+    db.session.commit()
+    return
 
 def create_message(content, user, group):
     msg = Message(content=content, sender=user, group=group, date=datetime.now())
@@ -56,7 +105,7 @@ def is_in_group(user_id, group_id):
     return db.session.query(group_user_table).filter_by(group_id=group_id, user_id=user_id).first() is not None
 
 @login_required
-@app.route('/t/<group_id>', methods=['POST','GET'])
+@app.route('/<group_id>', methods=['POST','GET'])
 def messages(group_id):
     groups = Group.query.all()
     active_group = Group.query.filter_by(id=group_id).one()
@@ -70,8 +119,8 @@ def messages(group_id):
             user=current_user,
             group=active_group)
     #GET method
-    return flask.render_template("messages.html.jinja2",
-                                 groups=groups,  active_group=active_group, errors = errors)
+    return flask.render_template("main_view.html.jinja2",
+                                 groups=groups, active_group=active_group, errors=errors)
 
 
 @app.route('/login', methods=['POST','GET'])
@@ -84,18 +133,33 @@ def login():
     if form_is_valid:
         user = User.query.filter_by(id=form.get('user_id')).first()
         user.authenticated = True
+        db.session.add(user)
+        db.session.commit()
         login_user(user)
         return redirect(url_for("messages", group_id=1))
 
-    return flask.render_template("logintest.html.jinja2", form=form, errors=errors)
+    users = User.query.all()
 
-@app.route('/debug/users', methods=['POST','GET'])
+    return flask.render_template("logintest.html.jinja2", users=users, errors=errors)
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+    logout_user()
+    return login()
+
+@app.route('/debug/users', methods=['GET','POST'])
 def debug_users():
 
     form = flask.request.form
-    create_user(
-        name=form.get('user_name')
-    )
+    if flask.request.method=='POST':
+        create_user(
+            name=form.get('user_name')
+        )
     users = User.query.all()
     return flask.render_template('debug/users.html.jinja2', users=users)
 
@@ -103,12 +167,14 @@ def debug_users():
 def debug_groups():
 
     form = flask.request.form
-    create_group(
-        name=form.get('group_name')
-    )
+    if flask.request.method=='POST':
+        create_group(
+            name=form.get('group_name')
+        )
     groups = Group.query.all()
     return flask.render_template('debug/groups.html.jinja2', groups=groups)
 
+@app.route('/debug/part', methods=['POST','GET'])
 @app.route('/debug/participations', methods=['POST','GET'])
 def debug_participations():
 
@@ -123,8 +189,9 @@ def debug_participations():
     users = User.query.all()
     return flask.render_template('debug/participations.html.jinja2', users=users, errors=errors)
 
-@app.route('/debug/messages')
-def debug_send():
+@app.route('/debug/msg', methods=['POST','GET'])
+@app.route('/debug/messages', methods=['POST','GET'])
+def debug_messages():
 
     form = flask.request.form
     form_is_valid, errors = is_debug_msg_form_valid(form)
@@ -140,42 +207,17 @@ def debug_send():
 
     groups = Group.query.all()
 
-    return flask.render_template('debug/msg.html.jinja2', groups=groups)
+    return flask.render_template('debug/messages.html.jinja2', groups=groups)
 
 #form is invalid if: user or group doesn't exist, user is not in group, msg is empty
 def is_msg_form_valid(form):
     result = True
     errors = []
 
-
-    # user_id = form.get('user_id', "")
-    # if user_id == "":
-    #     result = False
-    #     errors += ['user field is empty']
-    # else:
-    #     user = User.query.filter_by(id=user_id).first()
-    #     if user == "":
-    #         result = False
-    #         errors += ['user does not exist']
-
-    group_id = form.get('group_id', "")
-    if group_id == "":
-        result = False
-        errors += ['group field is empty']
-    else:
-        group = Group.query.filter_by(id=group_id).first()
-        if group == "":
-            result = False
-            errors += ['group does not exist']
-
     content = form.get('msg', "")
     if content == "":
         result = False
         errors += ['msg field is empty']
-
-    # if result and not is_in_group(user_id, group_id):
-    #     result = False
-    #     errors += ['user in not in group']
 
     print("############", errors)
     return result, errors
